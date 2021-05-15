c4e852985e1f41c67b80af1df5d8429b2087c82a
zengyan <zeng312yan@sina.com>
2021-05-15 15:37:15 +0800

    git test

---
+++ src/test/java/com/cn/MyTest.java
@@ -0,0 +1,20 @@
+package com.cn;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MyTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        System.out.println("aaaaaa");
+        System.out.println("bbbbbb");
+        System.out.println("cccccccccccc");
+
+    }
+}
